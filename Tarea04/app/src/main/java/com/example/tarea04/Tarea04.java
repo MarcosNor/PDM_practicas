e9dfb1c7264686715fea05690187d75e47ab7dfa
MarcosNor <noriegamarcos44@ciencias.unam.mx>
2024-04-27 15:31:02 -0600

    Creando proyecto, ahora queda cambiar las interfaces

---
+++ Tarea04/app/src/main/java/com/example/tarea04/Tarea04.java
@@ -0,0 +1,13 @@
+package com.example.tarea04;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tarea04 extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout01);
+    }
+}
